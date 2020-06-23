409a56e2b19eb5d6accb41cd4bb505bdae431017
Olimov Sulaymon <sulaymonlucky@gmail.com>
2020-06-23 23:42:16 +0500

    Working on Blog section and learning useEffect()

--- src/components/Dashboard/Todo/Todo.js
+++ src/components/Dashboard/Todo/Todo.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from '@material-ui/core/Table';
 import Paper from '@material-ui/core/Paper';
 import TableBody from '@material-ui/core/TableBody';
@@ -32,6 +32,10 @@ function Todo (props) {
     const theme = useTheme();
     const classes = useStyles();
     const [open, setOpen] = useState(false);
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+      document.title = `you cilckec page ${count} times.`;
+    })
     const handleClickOpen = () => {
       setOpen(true);
     };
@@ -53,8 +57,9 @@ function Todo (props) {
       props.onDeleteTodo(id)
     }
     return (
-      <Fragment>
-          <div style={{display: 'flex', justifyContent: 'space-between', marginBottom: 10}}>
+      <>
+          <div onClick={() => setCount(count + 1)}
+               style={{display: 'flex', justifyContent: 'space-between', marginBottom: 10}}>
           <Typography variant='h4'>
            My Todo List
           </Typography>
@@ -99,7 +104,7 @@ function Todo (props) {
               </TableBody>
             </Table>
           </TableContainer>
-      </Fragment>
+      </>
     );
 }
 
